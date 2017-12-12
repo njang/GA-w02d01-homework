185a3ebb2a3d14e00fd923df724584c521047843
njang <neo.w.jang@gmail.com>
2017-12-11 20:08:12 -0600

    1st commit - selecting datatypes

---
+++ answers.js
@@ -0,0 +1,17 @@
+
+// ### Answer the Following
+// For each of the following, write which **datatypes** you would use to represent the data, and then give a small example of the **data structure**:
+
+// 1. A light switch that can be either on or off.
+// boolean
+// 2. A user's email address.
+// string
+// 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// object
+// 4. A list of student names from our class.
+// array
+// 5. A list of student names from our class, each with a location.
+// array
+// 6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// array
+
