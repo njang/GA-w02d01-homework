330cfeb64cda7c5811dce2e91ceb7a1b020690c1
njang <neo.w.jang@gmail.com>
2017-12-11 20:26:42 -0600

    4th commit - accessed limbo

--- answers.js
+++ answers.js
@@ -68,3 +68,24 @@ console.log(crazyObject.larry.characters[2].favourtieHobby);
 console.log(crazyObject.larry.nicknames[1]);
 // 5. The object the contains the name `funkhauser`
 console.log(crazyObject.larry.characters[1].name);
+
+// Object-ception
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4);
